393ad8caea45c6878b8b0a7ec87837e3cb6d8650
Sambit Kumar Swain <swainsambitkumar59@gmail.com>
2025-01-17 00:10:27 +0530

    User COntroller And Routes set up

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ app.use(express.json({ limit: "15kb" })); // Parse incoming JSON requests with a
 app.use(express.urlencoded({ extended: true, limit: "15kb" })); // Parse URL-encoded requests with extended option and size limit
 app.use(express.static("public")); // Serve static files from the 'public' folder
 app.use(cookieParser()); // Parse cookies and make them understandable to the server
+//Routes Import:{We can Import these in }
+import userRouter from './routes/userRoutes.js';
+
+//Routes Declaration
+app.use("/api/v1/users",userRouter);// if i call userrouter using this template my link is"https/localhost:8000/api/v1/users/registeror login this type my url is being generated";
+
 
 export { app }; // New ES module syntax for exporting the Express app
 //7hr 32min 
