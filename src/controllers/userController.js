a7ad1782c5664cd6ea4376972368d68b64d7148f
Sambit Kumar Swain <swainsambitkumar59@gmail.com>
2025-01-28 15:37:30 +0530

    get channel details and get wtch history and route setup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,7 @@ import { User } from "../models/userModel.js";
 import {uploadOnCloudinary} from "../utils/cloudNary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt  from "jsonwebtoken";
+import mongoose from "mongoose";
 //access and refresh token generation for login
 const generateAccessAndRefreshTokens=async(userId)=>{
     try 
@@ -372,6 +373,64 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
         }
         return res.status(200).json(new ApiResponse(200,channel[0],"user channel fetched Successfully"));
 });
+//check User watch History
+const getWatchHistory=asyncHandler (async(req,res)=>{
+    const user=await User.aggregate(
+        [
+        {
+            $match:
+            {
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        }
+        ,{
+            $lookup:
+            {
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id", 
+                as:"watchHistory",
+                pipeline:
+                [
+                    {
+                        $lookup:
+                        {
+                            from:"users",
+                            localField:"videoUploader",
+                            foreignField:"_id",
+                            as:"videoUploader",
+                            pipeline:
+                            [
+                                {
+                                    $project:
+                                    {
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+        }
+    },{
+        $addFields:
+        {
+            owner:{
+                $first:"$owner"
+            }
+        }
+    }
+]
+}
+}
+]
+}
+}
+
+    ]
+)
+
+return res.status(200).json(
+    new ApiResponse(200,user[0].watchHistory,"Watch history ffetched successfully")
+);
+});
+
 
 export {
     registerUser,
@@ -383,5 +442,5 @@ export {
     updateUserDetails,
     updateAvatar,
     updateCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,getWatchHistory
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/userController.js";
+import { 
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    getCurrentUser,
+    changeCurrentPassword,
+    updateUserDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } from "../controllers/userController.js";
 import {upload} from '../middlewares/multerMiddleware.js';
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 const router=Router();
@@ -18,6 +29,22 @@ router.route("/register").post(
     registerUser);//here upload is a middleware function of multermiddleware
 //Login Router
 router.route("/login").post(loginUser);
+//Logout router
 router.route("/logout").post(verifyJWT,logoutUser);//here verifyJwt is the function of the authmiddleware
-router.route("refresh_token").post(refreshAccessToken);
+//Refresh router
+router.route("/refresh_token").post(refreshAccessToken);
+//Change password
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+//current user
+router.route("/current_user").get(verifyJWT,getCurrentUser);
+//update Account details
+router.route("/update-account").patch(verifyJWT,updateUserDetails);
+//update avatar
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateAvatar);
+//update coverIMage
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateCoverImage);
+//get channel details
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile);
+//get watchhistory
+router.route("/history").get(verifyJWT,getWatchHistory);
 export default router;
